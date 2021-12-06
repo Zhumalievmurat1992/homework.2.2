c39824523dd8e96c0cc01c896c20398897e96cd3
User <Zhumalievmurat1992@gmail.com>
2021-12-07 01:33:51 +0600

    изменил

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,24 +5,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        creatObject("mers").print();
+       /* creatObject("mers").print();
         creatObject("bmv").print();
-        creatObject("audi").print();
-        Printable [] name=new Printable[2];
+        creatObject("audi").print();*/
+        Printable[] printables ={ creatObject("MERS"),creatObject("BMV"),creatObject("AUDI") };
+        for (int i = 0; i < printables.length; i++) {
+            printables[i].print();
+            
+        }
 
-    }
+
+        }
+
+
 
     public static Printable creatObject(String Car) {
         Printable printable = null;
 
         switch (Car) {
-            case "mers":
-                printable = new Mers("MERS", 2.4);
+            case "MERS":
+                printable  = new Mers("MERS", 2.4);
                 break;
-            case "bmv":
-                printable = new Bmv("BMV"," red ", 2019);
+            case "BMV":
+                printable= new Bmv("BMV"," red ", 2019);
                 break;
-            case "audi":
+            case "AUDI":
 
                 printable = new Audi("AUDI"," cupe", " x5");
                 break;
